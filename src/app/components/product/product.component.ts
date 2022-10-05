c7dabcd48570ff920ec10b11be360fce5e100ae8
Ömer Faruk LALE <omerlaleee@gmail.com>
2022-10-05 19:38:30 +0300

    Request Structure was created by HttpClient

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -7,23 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1 = { productId: 1, productName: "Bardak", categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 1, productName: "Laptop", categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 1, productName: "Mouse", categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 1, productName: "Keybord", categoryId: 1, unitPrice: 5 }
-  product5 = { productId: 1, productName: "Camera", categoryId: 1, unitPrice: 5 }
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5
+  apiUrl = "https://localhost:44379/api/products/getall";
+  products: Product[] = [
   ]
-
-  constructor() { }
-
+  constructor(private httpClient: HttpClient) { }
   ngOnInit(): void {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
